6b834c68aa8e48db8acdd8fa0c78572f11504b3d
Kostiantyn Rybnikov <k-bx@k-bx.com>
2019-05-04 22:21:00 +0300

    Towards rendering Iso

--- src/Main.elm
+++ src/Main.elm
@@ -16,6 +16,16 @@ type Msg
     = NoOp
 
 
+type Expr
+    = Var String
+    | Sum Expr Expr
+    | Prod Expr Expr
+    | Def Expr Expr
+    | Params Expr (List Expr)
+    | OfType Expr Expr
+    | TFun Expr Expr
+
+
 init : () -> ( Model, Cmd Msg )
 init _ =
     ( {}, Cmd.none )
@@ -28,12 +38,61 @@ update msg model =
             ( model, Cmd.none )
 
 
+r : Expr -> String
+r x =
+    case x of
+        Var s ->
+            s
+
+        Sum e1 e2 ->
+            "\\sum_{" ++ r e1 ++ "}" ++ r e2
+
+        Prod e1 e2 ->
+            "\\prod_{" ++ r e1 ++ "}" ++ r e2
+
+        Def e1 e2 ->
+            r e1 ++ " :\\equiv " ++ r e2
+
+        Params name ps ->
+            r name ++ "(" ++ String.concat (List.intersperse "," (List.map r ps)) ++ ")"
+
+        OfType e1 e2 ->
+            r e1 ++ " : " ++ r e2
+
+        TFun e1 e2 ->
+            r e1 ++ " \\rightarrow " ++ r e2
+
+
 view : Model -> Html Msg
 view model =
-    div [ class "container" ]
-        [ h1 [] [ text "Hey" ]
-        , div []
-            [ iframe [srcdoc """
+    let
+        formulas =
+            [ Sum (Var "x") (Var "y")
+            , Def (Params (Var "Iso") [ Var "A", Var "B" ])
+                (Sum
+                    (OfType (Var "f")
+                        (TFun (Var "A")
+                            (Var "B")
+                        )
+                    )
+                    (Sum
+                        (OfType
+                            (Var "g")
+                            (TFun (Var "B") (Var "A"))
+                        )
+                        (Var "TODO")
+                    )
+                )
+            ]
+
+        renderFormula exp =
+            div [ class "row" ]
+                [ div [ class "col-md-6" ]
+                    [ text <| r exp ]
+                , div [ class "col-md-6" ]
+                    [ iframe
+                        [ srcdoc <|
+                            """
 <html>
   <head>
     <script type="text/x-mathjax-config">
@@ -42,14 +101,23 @@ view model =
     <script src='https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML' async></script>
   </head>
   <body>
-    When $a \\ne 0$, there are two solutions to \\(ax^2 + bx + c = 0\\) and they are
-    $$x = {-b \\pm \\sqrt{b^2-4ac} \\over 2a}.$$
+$$
+"""
+                                ++ r exp
+                                ++ """
+$$
   </body>
 </html>
-                              """]
-                  []
-            ]
+                              """
+                        ]
+                        []
+                    ]
+                ]
+    in
+    div [ class "container" ] <|
+        [ h1 [] [ text "Hey" ]
         ]
+            ++ List.map renderFormula formulas
 
 
 subscriptions : Model -> Sub Msg
